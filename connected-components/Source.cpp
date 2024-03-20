01267ddef55d8c0eae1b75f872459f98ab34ec13
Robin WOOTTON <robin.wootton@alstomgroup.com>
2024-03-20 14:05:12 +0000

    Extend graph; add diagram

--- connected-components/Source.cpp
+++ connected-components/Source.cpp
@@ -5,30 +5,37 @@
 #include <map>
 #include <set>
 
+// Saves re-typing
+std::string filename[] {
+    "SCC.txt",
+    "test_pairs.txt",
+    "test_pairs_2.txt"
+};
+
 int main() {
     /*
-    Map of node to nodes reachable
+    Map each node with outgoing connection, to nodes reachable
     */
     std::map<int, std::vector<int>*> graph;
 
-    short mostNumberOFConns = 1;
+    short mostNumberOfConns = 1; // Stats
 
     /*
     Open, read and close the File
     */
-    std::ifstream inputFile("SCC.txt");
+    std::ifstream inputFile(filename[2]);
     if (!inputFile) {
         std::cerr << "Error opening file." << std::endl;
         return 1;
     }
-
     int firstInt, secondInt;
     while (inputFile >> firstInt >> secondInt) {
         if (graph.contains(firstInt)) {
             graph[firstInt]->push_back(secondInt);
-            // Check quantity of links
-            short numConns = graph[firstInt]->size();
-            mostNumberOFConns = std::max(numConns, mostNumberOFConns);
+
+            // Stats
+            short numConns = short(graph[firstInt]->size());
+            mostNumberOfConns = std::max(numConns, mostNumberOfConns);
         }
         else {
             std::vector<int>* v = new std::vector<int>{ secondInt };
@@ -37,7 +44,8 @@ int main() {
     }
     inputFile.close();
 
-    std::cout << mostNumberOFConns << std::endl;
+    std::cout << "The most outgoing conns is " << mostNumberOfConns << std::endl; // Stats
+
     //// Access elements
     //for (const auto& t : myVector) {
     //    int intValue = std::get<0>(t);
